a51d16b7a6a3d56ccc4a71909cf2f906d9af875f
Arnav Kumar Gupta <arnavkumargupta777@gmail.com>
2025-08-22 20:22:40 +0530

    add equip model

---
+++ Backend/src/models/EquipmentSchema.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const equipmentSchema = new mongoose.Schema(
+  {
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: [true, "Equipment name is required"],
+      trim: true,
+    },
+    type: {
+      type: String,
+      enum: ["rent", "sale"],
+      required: [true, "Type must be 'rent' or 'sale'"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [1, "Price must be at least 1"],
+    },
+    location: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    available: {
+      type: Boolean,
+      default: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+    category: {
+      type: String,
+      enum: ["Tractor", "Plough", "Harvester", "Sprayer", "Other"],
+      default: "Other",
+    },
+    postedOn: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Equipment", equipmentSchema);
